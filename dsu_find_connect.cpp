43fc32426d44af3c96f77fcd58cdba544a8ffc22
Rasel Hasan <raselhasan.cs@gmail.com>
2021-12-11 23:39:01 +0600

    Create dsu_find_connect.cpp

---
+++ dsu_find_connect.cpp
@@ -0,0 +1,14 @@
+vector<int> parent(100005);
+
+int findparent(int u) {
+    if(u == parent[u]) return u;
+    return parent[u] = findparent(parent[u]);
+}
+
+void connect(int u, int v) {
+    u = findparent(u);
+    v = findparent(v);
+
+    if(u != v) parent[u] = v;
+    else flag = 0;
+}
